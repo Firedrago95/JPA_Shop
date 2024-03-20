9fde6eca2ae2fa2193dd4cf7dfac1ab86ccdb5f4
YONGHWA\ekore <yonghwa95@naver.com>
2024-03-20 11:12:33 +0900

    컬렉션은 필드에서 초기화하자

--- src/main/java/jpabook/domain/Member.java
+++ src/main/java/jpabook/domain/Member.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -21,5 +22,5 @@ public class Member {
     private Address address;
 
     @OneToMany(mappedBy = "member")
-    private List<Order> orders;
+    private List<Order> orders = new ArrayList<>();
 }
