20df3138ca9446ea9e44d37d9cc5afa033d21093
YONGHWA\ekore <yonghwa95@naver.com>
2024-03-20 11:07:32 +0900

    @XToOne 반드시 지연로딩(LAZY) 사용

--- src/main/java/jpabook/domain/Order.java
+++ src/main/java/jpabook/domain/Order.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -14,18 +16,21 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate; // 주문시간
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDRER, CANCEL]
+
+    public Order() {
+    }
 }
