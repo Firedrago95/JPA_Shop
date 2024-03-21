03ea748661eeb157e673de45acbfc67a5d50e89a
YONGHWA\ekore <yonghwa95@naver.com>
2024-03-21 10:47:21 +0900

    itemService, ItemRepository 작성

--- src/main/java/jpabook/domain/item/Item.java
+++ src/main/java/jpabook/domain/item/Item.java
@@ -3,6 +3,7 @@ package jpabook.domain.item;
 import jakarta.persistence.*;
 
 import jpabook.domain.Category;
+import jpabook.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
