18878660dfe9fd5edc68a68e6002f40ef707cbf1
YONGHWA\ekore <yonghwa95@naver.com>
2024-03-21 10:29:19 +0900

    @RunWhith @SpringBootTest 통합됨

--- src/test/java/jpabook/service/MemberServiceTest.java
+++ src/test/java/jpabook/service/MemberServiceTest.java
@@ -4,15 +4,11 @@ import jpabook.domain.Member;
 import jpabook.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 class MemberServiceTest {
