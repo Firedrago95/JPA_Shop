58823f7196b6cc3916dec1fa32233bf236fe4901
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-08 12:40:51 +0900

    회원 실습 복습

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("Kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        assertThat(memberService.findOne(saveId)).isEqualTo(member);
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("Kim");
+
+        Member member2 = new Member();
+        member2.setName("Kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
