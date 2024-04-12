e72c76608dd6fa0b89aae49aa61a8f560aaae69e
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-12 10:24:13 +0900

    OrderRepository 메서드 추가

---
+++ src/main/java/jpabook/api/OrderSimpleApiController.java
@@ -0,0 +1,35 @@
+package jpabook.api;
+
+import jpabook.domain.Order;
+import jpabook.domain.OrderSearch;
+import jpabook.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ *
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ *
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        orderRepository.findAllByString(new OrderSearch());
+    }
+}
