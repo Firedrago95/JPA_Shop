17448bec479d1201dd2d668fc6b83e95d64ca9fb
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-12 12:02:11 +0900

    간단한 주문조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/api/OrderSimpleApiController.java
@@ -1,21 +1,25 @@
 package jpabook.api;
 
+import jpabook.domain.Address;
 import jpabook.domain.Order;
 import jpabook.domain.OrderSearch;
+import jpabook.domain.OrderStatus;
 import jpabook.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 /**
- *
  * xToOne(ManyToOne, OneToOne) 관계 최적화
  * Order
  * Order -> Member
  * Order -> Delivery
- *
  */
 @RestController
 @RequiredArgsConstructor
@@ -37,5 +41,38 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        // Order 2개 조회
+        // N + 1 -> 1 + 회원 N(2) + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // Lazy 초기화
+        }
     }
 }
