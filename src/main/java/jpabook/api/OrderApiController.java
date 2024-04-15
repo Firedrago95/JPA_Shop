8ca977271959b961d107a9892989a29ad1d2461b
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-15 12:20:18 +0900

    주문조회 V2 : 엔티티를 DTO로 변환

--- src/main/java/jpabook/api/OrderApiController.java
+++ src/main/java/jpabook/api/OrderApiController.java
@@ -17,6 +17,8 @@ import java.util.stream.Collectors;
  * - 트랜잭션 안에서 지연 로딩 필요
  * - 양방향 연관관계 문제
  *
+ * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용 X)
+ * - 트랜잭션 안에서 지연 로딩 필요
  */
 @RestController
 @RequiredArgsConstructor
@@ -41,5 +43,45 @@ public class OrderApiController {
         return  all;
     }
 
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
 
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(OrderItemDto::new)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 }
