7389d30286367925523303d3869cfb9488f56f02
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-17 12:16:30 +0900

    주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/repository/order/query/OrderQueryRepository.java
@@ -1,10 +1,13 @@
 package jpabook.repository.order.query;
 
 import jakarta.persistence.EntityManager;
+import jpabook.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,10 +21,10 @@ public class OrderQueryRepository {
      * 단건 조회에서 많이 사용하는 방식
      */
     public List<OrderQueryDto> findOrderQueryDtos() {
-        // 루트 조회 (toOne 코드를 모두 한번에 조회)
+        // 루트 조회 (toOne 코드를 모두 한번에 조회) -> query 1번 : 2개
         List<OrderQueryDto> result = findOrders();
 
-        // 루프를 돌면서 컬렉션 추가 (추가 쿼리 실행)
+        // 루프를 돌면서 컬렉션 추가 (추가 쿼리 실행) -> query 2번 (N + 1 문제)
         result.forEach(o -> {
             List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
             o.setOrderItems(orderItems);
@@ -48,11 +51,48 @@ public class OrderQueryRepository {
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
         return em.createQuery(
                 "select" +
-                        " jpabook.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " new jpabook.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
                         " join oi.item i" +
                         " where oi.order.id = :orderId", OrderItemQueryDto.class)
                 .setParameter("orderId", orderId)
                 .getResultList();
+    }
+
+    /**
+     * 최적화
+     * Query: 루트 1번, 컬렉션 1번
+     * 데이터를 한꺼번에 처리할 때 많이 사용하는 방식
+     */
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        // 루트 조회
+        List<OrderQueryDto> result = findOrders();
+
+        // orderItem 컬렉션을 MAP 한방에 조회
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+        // 루프를 돌면서 컬렉션 추가(추가 쿼리 실행 x)
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return result;
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                .map(OrderQueryDto::getOrderId)
+                .collect(Collectors.toList());
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+        "select" +
+                " new jpabook.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                " from OrderItem oi" +
+                " join oi.item i" +
+                " where oi.order.id in :orderIds" ,OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        return orderItems.stream()
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
     }
 }
