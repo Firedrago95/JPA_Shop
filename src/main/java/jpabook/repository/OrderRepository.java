fc53edca6ee59590bf38bc17e781f524aca91f71
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-18 11:52:07 +0900

    Query dsl

--- src/main/java/jpabook/repository/OrderRepository.java
+++ src/main/java/jpabook/repository/OrderRepository.java
@@ -1,17 +1,21 @@
 package jpabook.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
-import jpabook.domain.Member;
+import jpabook.domain.*;
 import jpabook.domain.Order;
-import jpabook.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static jpabook.domain.QMember.member;
+import static jpabook.domain.QOrder.order;
 
 @Repository
 @RequiredArgsConstructor
@@ -85,6 +89,37 @@ public class OrderRepository {
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); // 최대 1000
         return query.getResultList();
     }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return order.status.eq(statusCond);
+    }
+
+    private com.querydsl.core.types.Predicate nameLike(String nameCond) {
+        if (nameCond == null) {
+            return null;
+        }
+        return member.name.like(nameCond);
+    }
+
+
 
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery(
